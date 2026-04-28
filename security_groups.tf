5b8fc8b3743ab0fbd01b858d22efba73d357aa08
Jack Nelson <jacknelson2351@gmail.com>
2026-04-28 15:51:08 -0400

    security group fix

--- security_groups.tf
+++ security_groups.tf
@@ -21,6 +21,13 @@ resource "aws_security_group" "lambda" {
   name   = "hashtopolis-lambda"
   vpc_id = aws_vpc.main.id
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
